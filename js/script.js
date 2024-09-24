4709755792f9197596932b50d6f0eb9e661da20d
vika.ritter.03@gmail.com <vika.ritter.03@gmail.com>
2024-09-24 21:44:16 +0500

    feat: add image gallery dynamic with API

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const imagesWrapper = document.querySelector(".images");
+
+const apiKey = "A3V9J66Wq8vD4mzygAQswTL95xwo8BdFyVkpDY1ZUcziZuXVGtDRnpz9";
+const perPage = 15;
+let currentPage = 1;
+
+const generateHTML = (images) => {
+   imagesWrapper.innerHTML += images.map(img => 
+      `<li class="card">
+            <img src="${img.src.large2x}" alt="img">
+            <div class="details">
+               <div class="photographer">
+                  <i class="uil uil-camera"></i>
+                  <span>${img.photographer}</span>
+               </div>
+               <button>
+                  <i class="uil uil-import"></i>
+               </button>
+            </div>
+         </li>`
+   ).join("");
+}
+
+const getImages = (apiURL) => {
+   fetch(apiURL, {
+      headers: { Authorization: apiKey }
+   }).then(res => res.json()).then(data => {
+      generateHTML(data.photos);
+   })
+}
+
+getImages(`https://api.pexels.com/v1/curated?page=${currentPage}per_page=${perPage}`);
